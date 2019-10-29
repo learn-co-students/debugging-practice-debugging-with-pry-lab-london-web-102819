55b631a0992e338302883e7e71d449cd4129ea9d
andrew-lewell <andrew.lewell@gmail.com>
2019-10-29 14:44:27 +0000

    Done.

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,6 +1,7 @@
 class Person
   attr_accessor :name, :candy_lover
 
+
   def initialize(name, candy_lover)
     @name = name
     @candy_lover = candy_lover
@@ -26,4 +27,5 @@ class Person
         "not a ballin candy lover"
     end
   end
+end
 end
