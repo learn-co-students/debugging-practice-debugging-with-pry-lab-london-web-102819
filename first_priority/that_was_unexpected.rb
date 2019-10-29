e2d69ee96315284f521a8c5ad1edc1109d466014
siapankina <siapankina@gmail.com>
2019-10-29 13:56:22 +0000

    fixed all problems using pry

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,10 +1,15 @@
+require 'pry'
+
 class Person
   attr_accessor :name, :candy_lover
+
 
   def initialize(name, candy_lover)
     @name = name
     @candy_lover = candy_lover
+  
   end
+  
 
   def tall?
     true
@@ -24,6 +29,7 @@ class Person
     else
       10.times do
         "not a ballin candy lover"
+      end
     end
   end
 end
